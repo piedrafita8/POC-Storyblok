c60b082a7029b36efb70aa6c85691773e21c139b
Santiago Piedrafita <santiagopiedrafita@MacBook-Pro-de-Tecnico.local>
2023-05-08 15:18:34 -0300

    feat: create header and layout components

---
+++ components/Header.js
@@ -0,0 +1,12 @@
+const Header = () => {
+  return (
+    <div class="w-full">
+      <div class="bg-pencil-green">
+        <p class="w-fit m-auto py-2">🚚 Delivery in 2 working days</p>
+      </div>
+      <div></div>
+    </div>
+  );
+};
+
+export default Header;
