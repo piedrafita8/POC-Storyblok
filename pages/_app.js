52edb01076f118a06760fc000aa7e188d634773e
Santiago Piedrafita <santiagopiedrafita@MacBook-Pro-de-Tecnico.local>
2023-05-08 14:27:46 -0300

    feat: add storyblokInit function

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,24 @@
-import '@/styles/globals.css'
+import "@/styles/globals.css";
+
+import { storyblokInit, apiPlugin } from "@storyblok/react";
+import Feature from "../components/Feature";
+import Grid from "../components/Grid";
+import Page from "../components/Page";
+import Teaser from "../components/Teaser";
+
+const components = {
+  feature: Feature,
+  grid: Grid,
+  teaser: Teaser,
+  page: Page,
+};
+
+storyblokInit({
+  accessToken: "rmVOH4IsBsFXEEOgQZpJgAtt",
+  use: [apiPlugin],
+  components,
+});
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
