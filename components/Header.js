3ff0b80ca9f66a0536ff1c8fb490b082407578ea
Santiago Piedrafita <santiagopiedrafita@MacBook-Pro-de-Tecnico.local>
2023-05-09 11:01:57 -0300

    feat: create static header

--- components/Header.js
+++ components/Header.js
@@ -1,10 +1,27 @@
+import Link from "next/link";
+import Image from "next/image";
+import searchIcon from "../public/search.svg";
+import cartIcon from "../public/cart.svg";
+
 const Header = () => {
   return (
     <div class="w-full">
       <div class="bg-pencil-green">
         <p class="w-fit m-auto py-2">🚚 Delivery in 2 working days</p>
       </div>
-      <div></div>
+      <div class="flex justify-between items-center bg-header-purple py-6 px-6">
+        <img src="https://a.storyblok.com/f/88751/251x53/0d3909fe96/storyblok-primary.png" />
+        <div class="flex gap-5">
+          <Link href="/">Products</Link>
+          <Link href="/">Collections</Link>
+          <Link href="/">Contact</Link>
+          <Link href="/">About</Link>
+        </div>
+        <div class="flex gap-5">
+          <Image src={searchIcon} width={24} height={24} />
+          <Image src={cartIcon} width={24} height={24} />
+        </div>
+      </div>
     </div>
   );
 };
